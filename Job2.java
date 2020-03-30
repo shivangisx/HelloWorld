fe01cad3b4e859a0bd2f43cfab85c54af8549c1c
shivangisx <shivisx123@gmail.com>
2020-03-30 17:44:11 +0530

    added 3 jobs 1 , 2 & 3

---
+++ Job2.java
@@ -0,0 +1,10 @@
+public class Job2{
+
+	public static void main(String[] args){
+		
+		for(int i=1;i<10;i++){
+			System.out.println("Job2..."+i);
+		}
+	}
+
+}
---
+++ Job3.java
@@ -0,0 +1,10 @@
+public class Job3{
+
+	public static void main(String[] args){
+		
+		for(int i=1;i<10;i++){
+			System.out.println("job3..."+i);
+		}
+	}
+
+}
